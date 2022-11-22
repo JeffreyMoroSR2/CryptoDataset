b437bb79b11b9a37de3ef43380c04cb3381ca857
JeffreyMoroSR2 <jmorosr2@gmail.com>
2022-11-23 00:03:11 +0200

    Added Salsa20 (not working properly)

--- Libs/Salsa20/Salsa20.py
+++ Libs/Salsa20/Salsa20.py
@@ -4,38 +4,44 @@ from Libs.CryptoAbstract import Crypto_Stream
 
 class Salsa_20(Crypto_Stream):
 
-    def __init__(self, key, mode):
+    def __init__(self, key, mode, nonce='nonce_df', block_length=8):
         self.mode = mode
+        self.nonce = str.encode(nonce)
+        if len(self.nonce) != 8:
+            raise ValueError('Nonce should be 8 bytes')
+
+        self.block_length = block_length
+        if self.block_length != 8 and self.block_length != 16 and self.block_length != 32:
+            raise ValueError('Block should be 8/16/32 bytes')
 
         # key initialization
         self.key = str.encode(key)
-        if len(self.key) > 32:
-            raise ValueError('Key must be in range of 16-32 bytes')
-        elif len(self.key) < 16:
-            self.key += str.encode('0') * (16 - len(self.key))
+        if len(self.key) != 32:
+            raise ValueError('Key must be 32 bytes')
 
-        # Salsa20 object initialization (from Crypto.Cipher lib)
-        self.cipher = Salsa20.new(self.key)
-        self.nonce = self.cipher.nonce
+        self.cipher = Salsa20.new(self.key, nonce=self.nonce)
 
-    def _encrypt(self, data):
-        return self.nonce + self.cipher.encrypt(data)
+    def _encrypt(self, data, flag=False):
+        if flag:
+            return data
+        return self.cipher.encrypt(data)
 
     def _decrypt(self, data):
-        data_nonce = data[:8]
-        enc_data = data[8:]
-        cipher_dec = Salsa20.new(key=self.key, nonce=data_nonce)
-        return cipher_dec.decrypt(enc_data)
+        return self.cipher.decrypt(data)
 
     def encfile(self, file_path_in, file_path_out):
         file_read = open(file_path_in, 'rb')
         file_write = open(file_path_out, 'wb')
         integer_val = 0
+
         while 1:
-            temp = file_read.read()
+            temp = file_read.read(self.block_length)
             if not temp:
+                nonce = self._encrypt(self.nonce, True)
+                file_write.write(nonce)
                 break
-            temp = temp if len(temp) == 1 else temp + integer_val.to_bytes((1 - len(temp)), 'big')
+            temp = temp if len(temp) == self.block_length else \
+                temp + integer_val.to_bytes((self.block_length - len(temp)), 'big')
             temp_number = self._encrypt(temp)
             file_write.write(temp_number)
 
@@ -43,10 +49,13 @@ class Salsa_20(Crypto_Stream):
         file_read = open(file_path_in, 'rb')
         file_write = open(file_path_out, 'wb')
         integer_val = 0
+
+        # file_write.write(file_read.read()[:8])
         while 1:
-            temp = file_read.read(1)
+            temp = file_read.read(self.block_length)
             if not temp:
                 break
-            temp = temp if len(temp) == 1 else temp + integer_val.to_bytes((1 - len(temp)), 'big')
+            temp = temp if len(temp) == self.block_length else \
+                temp + integer_val.to_bytes((self.block_length - len(temp)), 'big')
             temp_number = self._decrypt(temp)
             file_write.write(temp_number)
